5eeaa40e741fe27dc9ae74433735653260f80eb3
aid4ntait <122576363+aid4ntait@users.noreply.github.com>
2024-01-11 10:07:22 -0800

    Added Monkey Stats and also Monkey Methods

--- Monkey.java
+++ Monkey.java
@@ -1,18 +1,32 @@
 public class Monkey
 {
-    public static int numOfMonkeys;
+    // Monkey stats
+    //public static int numOfMonkeys;
     private int attackRange;
     private double attackSpeed;
     private int attackDamage;
     private int cost  = 100;
 
-    public void MonkeyClass()
+    public void Monkey()
     {
-        numOfMonkeys++;
+        //numOfMonkeys++;
         attackDamage = 30;
         attackSpeed = 1.00;
         
     }
-    
+    public void inRange()
+    {
+        //late calculate distance
+        while(dist < attackRange)
+        {
+            attack();
+            wait();
+        }
+    }
+    public void attack()
+    {
+        // find the closest bloon and hit them for attackDamage
+    }
+   
 
 }
