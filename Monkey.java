2b95cc8efc5435e825739d78929536e5cc28a72b
aid4ntait <122576363+aid4ntait@users.noreply.github.com>
2024-01-11 09:56:26 -0800

    Added Monkey class

---
+++ Monkey.java
@@ -0,0 +1,18 @@
+public class Monkey
+{
+    public static int numOfMonkeys;
+    private int attackRange;
+    private double attackSpeed;
+    private int attackDamage;
+    private int cost  = 100;
+
+    public void MonkeyClass()
+    {
+        numOfMonkeys++;
+        attackDamage = 30;
+        attackSpeed = 1.00;
+        
+    }
+    
+
+}
