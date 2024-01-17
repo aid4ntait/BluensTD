e7553eb0d3de369ea7c78c8ac7ce8ba13dbe2f28
aid4ntait <atait26@astoriak12.org>
2024-01-16 16:46:08 -0800

    ShopPanel buffoonery

--- ShopPanel.java
+++ ShopPanel.java
@@ -6,37 +6,45 @@ import java.awt.event.*;
 public class ShopPanel extends JPanel
 {
     private GridLayout griddy; 
+    private ImageIcon dart, ninja, gattling, sniper, cannon;
     
     public ShopPanel(Player player) 
     {
+        dart = new ImageIcon("DartMonkey.png");
+        ninja = new ImageIcon("NinjaMonkey.png");
+        gattling = new ImageIcon("Gattling.png");
+        sniper = new ImageIcon("Sniper.png");
+        cannon = new ImageIcon("Cannon (2).png");
 
         //manifests the shops actual picture 
-        this.setBackground(Color.BLACK);
-        this.setPreferredSize(new Dimension(1280, 175));
+        this.setBackground(Color.WHITE);
+        this.setPreferredSize(new Dimension(1280, 125));
 
         // sets the layout of the shop to be a grid layout of one column and five rows 
-        griddy = new GridLayout(1, 5, 20, 10);
+        griddy = new GridLayout(1, 5, 50, 50);
+        
         this.setLayout(griddy);
 
         // adds all the buttons for the monkeys // they need their damage range and speed actually made 
-        this.addButton("Ninja Monkey", 150, 1, 1, 1);
-        this.addButton("Dartling Gunner Monkey", 700, 1 , 1, 1);
-        this.addButton("Sniper Monkey", 250, 1, 1, 1);
-        this.addButton("Dart Monkey", 100, 1, 1, 1);
-        this.addButton("Cannon", 300, 1, 1, 1);
+        this.addButton("Ninja Monkey", 150, 1, 1, 1, ninja);
+        this.addButton("Dartling Gunner Monkey", 700, 1 , 1, 1, gattling);
+        this.addButton("Sniper Monkey", 250, 1, 1, 1, sniper);
+        this.addButton("Dart Monkey", 100, 1, 1, 1, dart);
+        this.addButton("Cannon", 300, 1, 1, 1, cannon);
         
     }
 
     // creates a button with the info relative to the monkey it is supposed to be able to add. Then, the button is given a mouse listener. 
-    private void addButton(String name, int cost, int damage, int range, int speed)
+    private void addButton(String name, int cost, int damage, int range, int speed, Icon icon)
     {
 
         JButton button = new JButton();
         button.addMouseListener(new Mouse(name, cost, speed, range, damage));
-        button.setIcon(new ImageIcon("dartMonekySized.png"));
+        button.setForeground(Color.BLACK);
+        button.setIcon(icon);
         button.setToolTipText("Drag to Purchase " + name + " for " + cost + " Bananas.");
         add(button);
-    }
+    }   
 
     // checks wether or not the player has enough money and then uses addMonkey in GamePanel
     public static void monkeyBought(String name, int cost, int speed, int range, int damage, int x, int y)
@@ -70,17 +78,12 @@ public class ShopPanel extends JPanel
             this.name = name;
         }
 
-        public void mouseDragged(MouseEvent e)
-        {
-            xPosition = e.getX();
-            yPosition = e.getY();
-        }
-
         public void mouseReleased(MouseEvent f)
         {
             xPosition = f.getX();
             yPosition = f.getY();
             monkeyBought(name, cost, speed, range, damage, xPosition, yPosition);
+            System.out.println("Price " + cost + "\nName " + name + "\nAt " + xPosition + ": " + yPosition);
         }
     }
 }
